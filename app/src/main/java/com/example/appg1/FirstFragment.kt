a133e174bc97fce04bfe74917d1acbb4e8b232df
mpmarfisi <matimarfisi99@gmail.com>
2022-12-20 04:05:59 -0300

    Agrega app

---
+++ app/src/main/java/com/example/appg1/FirstFragment.kt
@@ -0,0 +1,49 @@
+package com.example.appg1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.appg1.databinding.FragmentFirstBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val viewModel: ItemViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            var actualFigure = 2;
+            viewModel.data.value = actualFigure
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/appg1/MainActivity.kt
@@ -0,0 +1,138 @@
+package com.example.appg1
+
+//import android.R
+import android.content.ClipData
+import com.example.appg1.R
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.appg1.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+
+class ItemViewModel : ViewModel() {
+    val data = MutableLiveData<Int>()
+}
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var lightSensor: Sensor
+    private lateinit var proxSensor: Sensor
+
+    var actualFigure = 1
+
+    private val viewModel: ItemViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        /*
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }*/
+        viewModel.data.observe(this, Observer { data ->
+            // Update the UI with the data
+            actualFigure = data
+        })
+
+        // Inicializacion luxometro y proximidad
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        proxSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, proxSensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onSensorChanged(event: SensorEvent) {
+        //val fragment = supportFragmentManager.primaryNavigationFragment
+
+        if (actualFigure == 1 && event.sensor.stringType == "android.sensor.light") {
+            val lux = event.values[0]
+            // Update the UI with the lux value
+            when{
+                lux in 1.0..1000.0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                lux > 1000.0 -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+            val tv_luz = findViewById<TextView>(R.id.textView_luz)
+            tv_luz.setText(String.format("%.2f", lux))
+        } else if (actualFigure == 2 && event.sensor.stringType == "android.sensor.proximity") {
+            val prox = event.values[0]
+            val tv_prox2 = findViewById<TextView>(R.id.textView_prox)
+            when{
+                prox in 0.0..2.0 -> tv_prox2.setText("Cerca")
+                prox in 2.0..6.0 -> tv_prox2.setText("Medio")
+                prox in 6.0..10.0 -> tv_prox2.setText("Lejos")
+            }
+            val tv_prox = findViewById<TextView>(R.id.textView_prox)
+            tv_prox.setText(String.format("%.2f", prox))
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //TODO("Not yet implemented")
+    }
+}
